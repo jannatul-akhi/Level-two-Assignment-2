35cac89f02ad1a32ed264fe5cf39cc44853a12b0
jannatul-akhi <jannatulferdous3608@gmail.com>
2024-06-01 00:10:55 +0600

    added route error message

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,26 @@
 import express, {Request, Response} from 'express';
 import { ProductRoutes } from './modules/products/product.route';
+import cors from "cors";
 import { OrderRoutes } from './modules/orders/order.route';
 const app = express()
 // const port = 3000
 
 // Parsers
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
-})
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(400).json({
+    success: false,
+    message: "Route not found",
+  });
+});
 
 export default app;
